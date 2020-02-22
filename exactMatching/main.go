2f47a3a006c16d66bceac5066add5a1878c9437d
JerryXie96 <hongchxie2-c@my.cityu.edu.hk>
2020-02-22 18:14:07 +0800

    test the bn256 add and add the code about encryption in user

--- exactMatching/main.go
+++ exactMatching/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 	"math/big"
 
 	"github.com/clearmatics/bn256"
@@ -9,10 +12,8 @@ import (
 
 // the key set of CA
 type KeySetOfCA struct {
-	X  *big.Int
-	g1 *bn256.G1
-	g2 *bn256.G2
-	gx *bn256.G2
+	X  *big.Int  // X is private
+	gx *bn256.G2 // gx is for server
 }
 
 // the key set of server
@@ -35,24 +36,49 @@ var (
 	userKeySet   KeySetOfUser
 )
 
-// Generate the key set in CA
+// keyGeneration(): Generate the key set in CA
 func keyGeneration() {
-	serverKeySet.Kb, _ = rand.Int(rand.Reader, bn256.Order) // Kb: one key for server
-	userKeySet.Ku, _ = rand.Int(rand.Reader, bn256.Order)   // Ku: one key for user
-	X := big.NewInt(0)                                      // X: the key held by CA
-	X = X.Add(serverKeySet.Kb, userKeySet.Ku)               // X=Kb+Ku. for all the users, X is the same
+	serverKeySet.Kb, _ = rand.Int(rand.Reader, bn256.Order)     // Kb: one key for server
+	userKeySet.Ku, _ = rand.Int(rand.Reader, bn256.Order)       // Ku: one key for user
+	caKeySet.X = big.NewInt(0)                                  // X: the key held by CA
+	caKeySet.X = caKeySet.X.Add(serverKeySet.Kb, userKeySet.Ku) // X=Kb+Ku. for all the users, X is the same
 
-	caKeySet.g1 = new(bn256.G1) // the calculation base in G1
-	caKeySet.g2 = new(bn256.G2) // the calculation base in G2
-
-	caKeySet.gx.ScalarMult(caKeySet.g2, X)   // gx: g^x is used for the encryption in user
-	userKeySet.gx.ScalarMult(caKeySet.g2, X) // gx: g^x is used for the encryption in user
+	caKeySet.gx = new(bn256.G2).ScalarBaseMult(caKeySet.X)   // gx: g^x is used for the encryption in user
+	userKeySet.gx = new(bn256.G2).ScalarBaseMult(caKeySet.X) // gx: g^x is used for the encryption in user
 
 	userKeySet.alpha, _ = rand.Int(rand.Reader, bn256.Order)  // alpha: the encryption key for user in encryption
 	serverKeySet.beta, _ = rand.Int(rand.Reader, bn256.Order) // beta: the encryption key for server in on-chain encryption
 
 	serverKeySet.S, _ = rand.Int(rand.Reader, bn256.Order) // S: the encryption key for server in on-chain encryption
 }
+
+// userEncryption(w string) (*bn256.G2 C1, *bn256.G2 C2): Input a keyword(string w) and return a ciphertext tuple (C1, C2)
+func userEncryption(w string) (*bn256.G2, *bn256.G2) {
+	kwByte := []byte(w)                                     // the byte array of keyword w
+	hashedKw := sha256.Sum256(kwByte)                       // hashed the keyword w
+	numHashedKw := new(big.Int).SetBytes(hashedKw[:])       // tranform the hashed byte array into big.num
+	ru, _ := rand.Int(rand.Reader, bn256.Order)             // r_u: the nonce of this encryption procedure
+	C1 := new(bn256.G2).ScalarBaseMult(ru)                  // C1=g_2^{r_u}
+	C1 = C1.Neg(C1)                                         // C1=g_2^{-r_u}
+	tempC1 := new(bn256.G2).ScalarBaseMult(numHashedKw)     // tempC1=g_2^{H(w)}
+	tempC1 = tempC1.ScalarMult(tempC1, userKeySet.alpha)    // tempC1=g_2^{H(w)a}
+	C1 = C1.Add(C1, tempC1)                                 // C1=g_2^{H(w)a-r_u}
+	C2 := new(bn256.G2).ScalarMult(userKeySet.gx, ru)       // C2=g_2^{x*r_u}
+	temp1C2 := new(bn256.G2).ScalarBaseMult(userKeySet.Ku)  // temp1C2=g_2^{Ku}
+	temp1C2 = temp1C2.Neg(temp1C2)                          // temp1C2=g_2^{-Ku}
+	temp1C2 = temp1C2.ScalarMult(temp1C2, ru)               // temp1C2=g_2^{-Ku*r_u}
+	temp2C2 := new(bn256.G2).ScalarBaseMult(userKeySet.Ku)  // temp2C2=g_2^{Ku}
+	temp2C2 = temp2C2.ScalarMult(temp2C2, numHashedKw)      // temp2C2=g_2^{Ku*H(w)}
+	temp2C2 = temp2C2.ScalarMult(temp2C2, userKeySet.alpha) // temp2C2=g_2^{Ku*H(w)*a}
+	C2 = C2.Add(C2, temp1C2)                                // C2=g_2^{Kb*ru}
+	C2 = C2.Add(C2, temp2C2)                                // C2=g_2^{Kb*ru}*g_2^{Ku*H(w)*a}
+	return C1, C2
+}
+
+// main(): the main entrance
 func main() {
 	keyGeneration()
+	c1, c2 := userEncryption("hello")
+	fmt.Println(hex.EncodeToString(c1.Marshal()))
+	fmt.Println(hex.EncodeToString(c2.Marshal()))
 }
---
+++ testProg/testadd.go
@@ -0,0 +1,57 @@
+package test
+
+import (
+	"bytes"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+
+	"github.com/clearmatics/bn256"
+)
+
+func main() {
+	a, _ := rand.Int(rand.Reader, bn256.Order)
+	b, _ := rand.Int(rand.Reader, bn256.Order)
+	c, _ := rand.Int(rand.Reader, bn256.Order)
+	d, _ := rand.Int(rand.Reader, bn256.Order)
+
+	g1a := new(bn256.G1).ScalarBaseMult(a) // g1^a
+	g1b := new(bn256.G1).ScalarBaseMult(b)
+	g1b = g1b.Neg(g1b) // g1^(-b)
+
+	g2c := new(bn256.G2).ScalarBaseMult(c) // g2^c
+	g2d := new(bn256.G2).ScalarBaseMult(d)
+	g2d = g2d.Neg(g2d) // g2^(-d)
+
+	g1a = g1a.Add(g1a, g1b) //g1^(a-b)
+	g2c = g2c.Add(g2c, g2d) //g2^(c-d)
+
+	ab := a.Sub(a, b)
+	cd := c.Sub(c, d)
+	g1ab := new(bn256.G1).ScalarBaseMult(ab)
+	g2cd := new(bn256.G2).ScalarBaseMult(cd)
+
+	fmt.Println(hex.EncodeToString(g1a.Marshal()))
+	fmt.Println()
+	fmt.Println(hex.EncodeToString(g1ab.Marshal()))
+	fmt.Println()
+
+	fmt.Println(hex.EncodeToString(g2c.Marshal()))
+	fmt.Println()
+	fmt.Println(hex.EncodeToString(g2cd.Marshal()))
+	fmt.Println()
+	k1 := bn256.Pair(g1a, g2c)
+	k2 := bn256.Pair(g1ab, g2cd)
+	k1Byte := k1.Marshal()
+	k2Byte := k2.Marshal()
+
+	fmt.Println(hex.EncodeToString(k1Byte))
+
+	fmt.Println(hex.EncodeToString(k2Byte))
+
+	if !bytes.Equal(k1Byte, k2Byte) {
+		fmt.Print("keys didn't agree")
+	} else {
+		fmt.Print("Agreed\n")
+	}
+}
